ec16087b5e08520bb09d36bd20e509a4b01d77c6
Bishal <bishal98gupta@gmail.com>
2025-08-13 17:04:31 +0530

    Added swipe functionality test along with POM file

--- test/pageobjects/swipe.page.ts
+++ test/pageobjects/swipe.page.ts
@@ -7,16 +7,39 @@ class swipePage extends HomePage {
   get secondCard() {
     return $('(//android.view.ViewGroup[@content-desc="card"])[2]');
   }
+  get foundMe() {
+    return $('android=new UiSelector().text("You found me!!!")');
+  }
 
-  async horizontalScrollToRight() {
-    await expect(this.firstCard).toBeExisting();
-    while (await this.secondCard.isDisplayed()) {
-      await driver.swipe({
-        direction: "left",
-        scrollableElement: this.firstCard,
-      });
-      await driver.pause(2500);
+  async swipeThroughCardsAndVerifyTexts(expectedTexts: string[]) {
+    //for fetching all textViews inside each cards
+    // const textViews = await this.firstCard.$$("//android.widget.TextView");
+    // for (const tv of textViews) {
+    //   const cardText = await tv.getText();
+    //   console.log("card text:", cardText);
+    // }
+
+    for (const expectedText of expectedTexts) {
+      const cardText = await this.firstCard
+        .$("(//android.widget.TextView)[2]")
+        .getText();
+      await expect(cardText).toMatch(expectedText);
+      if (await this.secondCard.isDisplayed()) {
+        // Swipe left on the first card
+        await driver.swipe({
+          direction: "left",
+          scrollableElement: this.firstCard,
+        });
+        await driver.pause(1000);
+      }
     }
   }
+
+  async scrollDownvalidation() {
+    await this.foundMe.scrollIntoView();
+    await expect(this.foundMe).toBeDisplayed();
+    await expect(this.foundMe).toHaveText("You found me!!!");
+  }
 }
+
 export default new swipePage();
