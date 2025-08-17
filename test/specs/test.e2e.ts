6608efb1c055b08bce67ab5a266eb8fbd363fd82
Bishal <bishal98gupta@gmail.com>
2025-08-17 12:42:37 +0530

    Added BS workflow and wdio config file

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -18,39 +18,36 @@ describe("Login Tests", () => {
   });
 });
 
-// describe("Form Tests", () => {
-//   const homepage = new HomePage();
-//   it("Fill up form values", async () => {
-//     await homepage.FormButton.click();
-//     await formPage.fillFormData("hello there!");
-//   });
-// });
+describe("Form Tests", () => {
+  const homepage = new HomePage();
+  it("Fill up form values", async () => {
+    await homepage.FormButton.click();
+    await formPage.fillFormData("hello there!");
+  });
+});
 
-// describe("Swipe Tests", () => {
-//   const homepage = new HomePage();
-//   it("Horizontal left swipes", async () => {
-//     await homepage.swipeButton.click();
-//     await swipePage.swipeThroughCardsAndVerifyTexts([
-//       "FULLY OPEN SOURCE",
-//       "GREAT COMMUNITY",
-//       "JS.FOUNDATION",
-//       "SUPPORT VIDEOS",
-//       "EXTENDABLE",
-//     ]);
-//     await swipePage.scrollDownvalidation();
-//   });
-// });
+describe("Swipe Tests", () => {
+  const homepage = new HomePage();
+  it("Horizontal left swipes", async () => {
+    await homepage.swipeButton.click();
+    await swipePage.swipeThroughCardsAndVerifyTexts([
+      "FULLY OPEN SOURCE",
+      "GREAT COMMUNITY",
+      "JS.FOUNDATION",
+      "SUPPORT VIDEOS",
+      "EXTENDABLE",
+    ]);
+    await swipePage.scrollDownvalidation();
+  });
+});
 
-// describe("drag & drop Tests", () => {
-//   const homepage = new HomePage();
-//   it("drag & drop", async () => {
-//     await homepage.dragButton.click();
-//     await dragPage.dragAndDropElement();
-//     await dragPage.verifySuccessMessage(
-//       "You made it, click retry if you want to try it again."
-//     );
-//   });
-// });
-
-//let me tell you, i dont wan tto make any changes to my wdio file, since in my local, the apk, will be avialble at the same path. But only for the workflow run, i wan tot use that download apk link and pass it to the workflow during runtime from the GH 'Run workflow' UI
-//bs://bc8c44ac69a9f0292ab942453817401ce4c4bbf4
+describe("drag & drop Tests", () => {
+  const homepage = new HomePage();
+  it("drag & drop", async () => {
+    await homepage.dragButton.click();
+    await dragPage.dragAndDropElement();
+    await dragPage.verifySuccessMessage(
+      "You made it, click retry if you want to try it again."
+    );
+  });
+});
